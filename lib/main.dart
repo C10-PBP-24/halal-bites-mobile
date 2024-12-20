d70b1e3751afca43e4d1cacbd6cf6ac67d6fe652
emanuellaabygail <abygailsiahaan1911@gmail.com>
2024-12-20 21:39:06 +0700

    fix add and delete

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.deepPurple,
           ).copyWith(secondary: Colors.deepPurple[400]),
         ),
-        home: LoginPage(),
+        home: RestoPage(),
       ),
     );
   }
